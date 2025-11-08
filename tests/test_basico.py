bd6e576c2fd075280504cd5aca9cc2540e0c2386
Mars <lmatosmari.m@gmail.com>
2025-11-08 17:43:10 -0300

    Adicionado teste para listar tarefas em test_basico.py

--- tests/test_basico.py
+++ tests/test_basico.py
@@ -16,3 +16,21 @@ def test_criar_tarefa():
     assert resposta.status_code == 201
     dados = resposta.get_json()
     assert dados["title"] == "Estudar"
+
+def test_listar_tarefas():
+    client = app.test_client()
+
+    # cria uma tarefa
+    client.post(
+        "/tasks",
+        data=json.dumps({"title": "Tarefa X"}),
+        content_type="application/json"
+    )
+
+    # agora lista
+    resposta = client.get("/tasks")
+    assert resposta.status_code == 200
+
+    dados = resposta.get_json()
+    assert isinstance(dados, list)
+    assert len(dados) >= 1
