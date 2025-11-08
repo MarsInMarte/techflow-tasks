138e9bf323ab3f8721c3bf23566534057dcb986a
Mars <lmatosmari.m@gmail.com>
2025-11-08 17:26:46 -0300

    Inicializado a lista de tarefas e a próxima variável de ID.
    Adicionado uma lista para armazenar tarefas e uma variável para o ID da próxima tarefa.

--- src/app.py
+++ src/app.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# Lista onde vamos guardar as tarefas
+tasks = []
+next_id = 1
+
 @app.get("/")
 def home():
     return "Olá! O sistema está funcionando!"
