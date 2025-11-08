75394827938eb797a9e2d00033d4faf83f70cd91
Mars <lmatosmari.m@gmail.com>
2025-11-08 17:50:49 -0300

    Implementado teste de exclusão de tarefas
    Adicionado teste para exclusão de tarefas na API.

--- tests/test_basico.py
+++ tests/test_basico.py
@@ -57,3 +57,20 @@ def test_atualizar_tarefa():
     assert resposta2.status_code == 200
     dados = resposta2.get_json()
     assert dados["title"] == "Novo título"
+
+def test_apagar_tarefa():
+    client = app.test_client()
+
+    # cria uma tarefa
+    resposta = client.post(
+        "/tasks",
+        data=json.dumps({"title": "Apagar"}),
+        content_type="application/json"
+    )
+    tarefa = resposta.get_json()
+    id_tarefa = tarefa["id"]
+
+    # apaga a tarefa
+    resposta2 = client.delete(f"/tasks/{id_tarefa}")
+
+    assert resposta2.status_code == 204
