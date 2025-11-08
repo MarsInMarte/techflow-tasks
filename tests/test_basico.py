fd0aaa1bdfd598787aac21dc6a36987178e2d5c6
Mars <lmatosmari.m@gmail.com>
2025-11-08 17:28:21 -0300

    Adicionado teste para criação de tarefas no aplicativo.

--- tests/test_basico.py
+++ tests/test_basico.py
@@ -1,2 +1,15 @@
-def test_exemplo():
-    assert 1 == 1
+import json
+from src.app import app
+
+def test_criar_tarefa():
+    client = app.test_client()
+
+    resposta = client.post(
+        "/tasks",
+        data=json.dumps({"title": "Estudar"}),
+        content_type="application/json"
+    )
+
+    assert resposta.status_code == 201
+    dados = resposta.get_json()
+    assert dados["title"] == "Estudar"
